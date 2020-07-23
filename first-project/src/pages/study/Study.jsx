c13a03011202267f53de8fb8f51d7ab42d8242f4
huangyichao0916 <939121836@qq.com>
2020-07-23 15:24:16 +0800

    进一步完善

--- first-project/src/pages/study/Study.jsx
+++ first-project/src/pages/study/Study.jsx
@@ -1,14 +1,41 @@
-import React, { Component } from 'react';
+import React, { } from 'react';
+import './study.styl'
+import { Header } from './studyHeader.style'
 
-class Study extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <div>
-                study
+function Study() {
+    return (
+        <div className="study">
+            <Header>
+                <span className="study-header-title">我的学习</span>
+                <span className="icon iconfont">&#xe615;</span>
+            </Header>
+            <div className="study-time">
+                本周时长<strong>0</strong>小时<strong>0</strong>分钟 <span className="icon iconfont">&#xe613;</span>
             </div>
-        );
-    }
+            <div className="study-record">
+                <div className="study-record-item notes">
+                    <div>0</div>
+                    <div>笔记</div>
+                </div>
+                <div className="study-record-item comments">
+                    <div>0</div>
+                    <div>留言</div>
+                </div>
+                <div className="study-record-item collection">
+                    <div>0</div>
+                    <div>收藏</div>
+                </div>
+                <div className="study-record-item download">
+                    <div>0</div>
+                    <div>下载</div>
+                </div>
+            </div>
+            <div className="study-content-wrapper">
+                <div className="no-study-record">暂无学习内容</div>
+                <button className="subscribe-button">订阅</button>
+            </div>
+        </div>
+    );
 }
- 
+
 export default Study;
