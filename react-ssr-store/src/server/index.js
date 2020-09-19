06b657806fbe35666e69c4202b28bfd846d541f5
huangyichao0916 <939121836@qq.com>
2020-09-19 20:37:21 +0800

    react-ssr

--- react-ssr-store/src/server/index.js
+++ react-ssr-store/src/server/index.js
@@ -4,21 +4,30 @@ import { renderToString } from 'react-dom/server';
 import Header from '../common/Header';
 
 const app = express();
+app.use(express.static('public'))
 app.get('*', (req, res) => {
-  const htmlStr = renderToString(<Header />)
-  res.end(`
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-  </head>
-  <body>
-    <div id="root">${htmlStr}</div>
-  </body>
-  </html>
-  `)
+  // 请求 是在 后端
+  Header.loadData().then(r => {
+    const htmlStr = renderToString(<Header />);
+    res.end(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+    </head>
+    <body>
+      <div id="root">${htmlStr}</div>
+      <script>window.songs = ${JSON.stringify(r.data)}</script>
+      <script src="/index.js"></script>
+    </body>
+    </html>
+    `)
+  })
+  
+  // <div>hello world</div>
+  
 })
 // 渲染 react 组件
 app.listen(9090, () => {
