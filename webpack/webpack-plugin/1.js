9d82c1ad2b0a41770567a44624d14baa26aac77d
huangyichao0916 <939121836@qq.com>
2020-11-16 14:21:25 +0800

    webpack study

--- webpack/webpack-plugin/1.js
+++ webpack/webpack-plugin/1.js
@@ -1,9 +1,27 @@
-const {SyncHook} = require('tapable');
+const {SyncHook, AsyncSeriesHook} = require('tapable');
 
 const hook = new SyncHook();
+const asyncHook = new AsyncSeriesHook();
 
 hook.tap('hook1',() => {
     console.log(1)
 })
-
 hook.call();
+
+asyncHook.tapPromise('hyc',() => {
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            resolve();
+        },2000)
+    })
+})
+asyncHook.tapPromise('asd',() => {
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            resolve();
+        },4000)
+    })
+})
+asyncHook.promise().then(() => {
+    console.log('async is over')
+})
---
+++ webpack/webpack-plugin/readme.md
@@ -0,0 +1,2 @@
+## babel插件和webpack loader的区别
+ - babel plugin会将代码转换成AST，而webpack loader是直接拿到代码
