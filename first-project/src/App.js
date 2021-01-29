78dee2d57c370b645e3bf1940e5c715f6c3e1adf
huangyichao0916 <939121836@qq.com>
2021-01-30 00:21:35 +0800

    test3

--- first-project/src/App.js
+++ first-project/src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import './App.styl';
-import './assets/font/iconfont.css'
-import { renderRoutes } from 'react-router-config';
+import React from "react";
+import "./App.styl";
+import "./assets/font/iconfont.css";
+import { renderRoutes } from "react-router-config";
 
 function App(props) {
   console.log(props);
-  console.log(window.history)
-  console.log(window.location)
-  return ( 
+  console.log(window.history);
+  console.log(window.location);
+  return (
     <div className="app">
       {renderRoutes(props.route.routes)}
       {/* <Footer /> */}
