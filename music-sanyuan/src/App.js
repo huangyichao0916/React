0f58a0aa069c7a448d319f3bcdec7e9b5b06f4b3
huangyichao0916 <939121836@qq.com>
2021-02-21 02:04:19 +0800

    prettier test

--- music-sanyuan/src/App.js
+++ music-sanyuan/src/App.js
@@ -1,57 +1,57 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 //123
 const chapterTree = {
-  name: "总章节",
-  children: [
-    {
-      name: "章节一",
-      children: [
+    name: '总章节',
+    children: [
         {
-          name: "第一节",
-          children: [
-            {
-              name: "第一小节",
-            },
-            {
-              name: "第二小节",
-            },
-          ],
+            name: '章节一',
+            children: [
+                {
+                    name: '第一节',
+                    children: [
+                        {
+                            name: '第一小节',
+                        },
+                        {
+                            name: '第二小节',
+                        },
+                    ],
+                },
+                {
+                    name: '第二节',
+                },
+            ],
         },
         {
-          name: "第二节",
+            name: '章节二',
+            children: [
+                {
+                    name: '第三节',
+                },
+                {
+                    name: '第四节',
+                },
+            ],
         },
-      ],
-    },
-    {
-      name: "章节二",
-      children: [
-        {
-          name: "第三节",
-        },
-        {
-          name: "第四节",
-        },
-      ],
-    },
-  ],
+    ],
 };
 
 const helper = (node) => {
-  return (
-    <ul>
-      <li>{node.name}</li>
-      {node.children &&
-        node.children.map((child) => {
-          return helper(child);
-        })}
-    </ul>
-  );
+    return (
+        <ul>
+            <li>{node.name}</li>
+            {node.children &&
+                node.children.map((child) => {
+                    return helper(child);
+                })}
+        </ul>
+    );
 };
 
 function App() {
-  return <div>{helper(chapterTree)}</div>;
+    return <div>{helper(chapterTree)}</div>;
 }
 
 export default App;
